4ea8395655e649be3ca2086bb8debf720abc38ed
Rau0622 <rau_moraes@hotmail.com>
2022-03-30 17:07:04 -0300

    Initial commit

---
+++ src/com/company/MetrosParaCm.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class MetrosParaCm {
+    public static void main(String[] args){
+
+    Scanner scanner = new Scanner(System.in) ;
+    System.out.print("Digite os metro:");
+     Double = scanner.nextDouble();
+
+  }
+}
